c5a2c37abd0f7e3696d27d10771377b9af9ea183
anshu <noobhack91@gmail.com>
2024-12-12 15:39:50 +0530

    aaa

--- server/routes/databaseRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { initializeDatabase } from '../controllers/databaseController.js';
-import { authenticate, authorize } from '../middleware/auth.js';
-
-const router = express.Router();
-
-router.post('/init', authenticate, authorize('admin'), initializeDatabase);
-
-export default router;
